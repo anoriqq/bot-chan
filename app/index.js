e42a246c657890c7df5a5835708944a4b48681c1
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-10-23 00:53:20 +0900

    眠い

--- app/index.js
+++ app/index.js
@@ -13,31 +13,34 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api', function(req, res){
+app.get('/api', function (req, res) {
   res.status(200).send('Hello World!');
 });
 
-app.post('/api', function(req, res){
+app.post('/api', function (req, res) {
   let body = req.body;
   console.log(body);
   let options = {
     url: 'https://slack.com/api/chat.postMessage',
-    headers: {
-      "Content-type": "application/json"
-    },
-    json: {
-      token: BOT_USER_OAUTH_ACCESS_TOKEN,
-      channel: body.event.channel,
-      text: body.event.text
-    }
+    headers: [
+      {
+        "Content-type": "application/json"
+      }
+    ],
+    json: [
+      {
+        "token": BOT_USER_OAUTH_ACCESS_TOKEN,
+        "channel": body.event.channel,
+        "text": body.event.text
+      }
+    ]
   };
   res.end();
-  request.post(options, function(error, response, body){
-    console.log(response.body);
+  request.post(options, function (error, response, body) {
     console.log(body);
   });
 })
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
   console.log('Node app is running at localhost:' + app.get('port'));
 })
