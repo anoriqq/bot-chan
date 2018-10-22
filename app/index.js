09a8bd4c9564422fc4ee9b4d713934687bc6381b
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-10-22 22:15:20 +0900

    ルート

--- app/index.js
+++ app/index.js
@@ -5,9 +5,13 @@ const app = express();
 app.set('port', (process.env.PORT || 8000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res){
-  res.send('Hello World!');
+app.get('/api', function(req, res){
+  res.status(200).send('Hello World!');
 });
+
+app.post('/', function(req, res){
+  console.log(req.body);
+})
 
 app.listen(app.get('port'), function(){
   console.log('Node app is running at localhost:' + app.get('port'));
