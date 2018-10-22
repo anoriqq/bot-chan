0a411a048de73d6bade6b1fa3c20cd69e3247f71
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-10-22 21:58:15 +0900

    hello worldを実装

--- app/index.js
+++ app/index.js
@@ -1,21 +1,14 @@
 'use strict';
-const http = require('http');
-const server = http.createServer((req, res) => {
-    let body = '';
-    req.on('data', (chunk) =>{
-      body += chunk;
-    });
-    req.on('end', () => {
-      console.log(body);
-      res.writeHead(200, {
-        'Content-Type': 'text/plain'
-      });
-      let challenge = JSON.parse(body).challenge;
-      res.write(challenge);
-      res.end();
-    });
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 8000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.send('Hello World!');
 });
-const port = process.env.PORT || 8000;
-server.listen(port, () => {
-  console.log('Listening on ' + port);
-});
+
+app.listen(app.get('port'), function(){
+  console.log('Node app is running at localhost:' + app.get('port'));
+})
