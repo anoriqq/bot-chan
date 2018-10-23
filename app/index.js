8ffa4d2541a3f69e34fafd9e0a7066353b94f51f
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-10-23 14:31:28 +0900

    オウム返しするようにした

--- app/index.js
+++ app/index.js
@@ -1,40 +1,37 @@
 'use strict';
-const http = require('http');
-const request = require('request');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
+// 環境変数の参照
 const OAUTH_ACCESS_TOKEN = process.env.OAUTH_ACCESS_TOKEN;
 const BOT_USER_OAUTH_ACCESS_TOKEN = process.env.BOT_USER_OAUTH_ACCESS_TOKEN;
 
-app.set('port', (process.env.PORT || 8000));
-app.use(express.static(__dirname + '/public'));
+// モジュールの読み込み
+const http = require('http');
+const nodeFetch = require('node-fetch');
+const request = require('request');
+const express = require('express');
+const formUrlencoded = require('form-urlencoded');
+const bodyParser = require('body-parser');
+const app = express();
 
+// Hrokuのポートか8000番ポートでサーバーを作る
+app.set('port', (process.env.PORT || 8000));
+
+// bodyをそれぞれの形式で読み込むように設定
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api', function (req, res) {
-  res.status(200).send('Hello World!');
-});
-
+// /apiにPOSTメゾットが来たときの処理
 app.post('/api', function (req, res) {
-  console.log(req.body);
-  if(req.body.challenge !== undefined){
+  if (req.body.challenge !== undefined) {
+    // challengeがあったら認証処理
     res.status(200).send(req.body.challenge).end();
+    console.log('Enable Events認証テスト challenge: ' + req.body.challenge);
+  } else if(req.body.event.bot_id == undefined){
+    let options = formUrlencoded(`?token=${BOT_USER_OAUTH_ACCESS_TOKEN}&channel=${req.body.event.channel}&text=${req.body.event.text}`);
+    nodeFetch(`https://slack.com/api/chat.postMessage${options}`);
   }
-  if (req.body.event.user == 'UAE9Z2JUF') {
-    request.post({
-      url: 'https://slack.com/api/chat.postMessage',
-      form: { token: BOT_USER_OAUTH_ACCESS_TOKEN, channel: req.body.event.channel, text: req.body.event.text }
-    },
-      function (err, httpResponse, body) {
-        console.log(body);
-      })
-  }
-  req.end();
 })
 
+// 指定ポートで起動が確認できたらログを出力する
 app.listen(app.get('port'), function () {
   console.log('Node app is running at localhost:' + app.get('port'));
 })
