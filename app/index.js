febde0c050778fd21b2c48a1f54dba22b9b2160a
show0317sw <33250012+show0317sw@users.noreply.github.com>
2018-10-23 14:40:14 +0900

    bug

--- app/index.js
+++ app/index.js
@@ -9,6 +9,7 @@ const nodeFetch = require('node-fetch');
 const request = require('request');
 const express = require('express');
 const formUrlencoded = require('form-urlencoded');
+const { URLSearchParams } = require("url");
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -19,15 +20,34 @@ app.set('port', (process.env.PORT || 8000));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function createURLSearchParams(data) {
+  const params = new URLSearchParams();
+  Object.keys(data).forEach(key => params.append(key, data[key]));
+  return params;
+}
+
 // /apiにPOSTメゾットが来たときの処理
 app.post('/api', function (req, res) {
   if (req.body.challenge !== undefined) {
     // challengeがあったら認証処理
     res.status(200).send(req.body.challenge).end();
     console.log('Enable Events認証テスト challenge: ' + req.body.challenge);
-  } else if(req.body.event.bot_id == undefined){
-    let options = formUrlencoded(`?token=${BOT_USER_OAUTH_ACCESS_TOKEN}&channel=${req.body.event.channel}&text=${req.body.event.text}`);
-    nodeFetch(`https://slack.com/api/chat.postMessage${options}`);
+  } else if (req.body.event.bot_id == undefined) {
+    const url = "https://slack.com/api/chat.postMessage";
+    const data = {
+      "token": BOT_USER_OAUTH_ACCESS_TOKEN,
+      "channel": req.body.event.channel,
+      "text": req.body.event.text
+    };
+    const params = createURLSearchParams(data);
+    const opt = {
+      method: "POST",
+      body: params
+    };
+    fetch(url, opt)
+      .then(res => res.text())
+      .then(text => console.log(text))
+      .catch(err => console.log(err));
   }
 })
 
